c3ff7a0e5853fb61bfeb197d63039dda8cd8b860
danny-jin <icarus96119@gmail.com>
2022-04-21 21:10:46 +0900

    update: port

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     }),
   );
   SwaggerModule.setup('docs', app, document);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 5000);
 }
 
-bootstrap();
+bootstrap().then();
